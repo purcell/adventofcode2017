32f67478389a9acb49a6472624407f2adee8e52e
Steve Purcell <steve@sanityinc.com>
2017-12-01 20:06:48 +1300

    Day 1b

--- src/Day01.hs
+++ src/Day01.hs
@@ -6,16 +6,26 @@ import Text.Parsec
 import Text.Parsec.String
 
 
-checksum :: (Eq a, Num a) => [a] -> a
-checksum [] = 0
-checksum ds@(d:_) = sum [d1 | (d1, d2) <- pairs (ds ++ [d]), d1 == d2]
-  where pairs :: [a] -> [(a,a)]
-        pairs xs = zip xs (tail xs)
 
+checksum :: (Eq a, Num a) => Int -> [a] -> a
+checksum _ [] = 0
+checksum offset ds = sum [d1 | (d1, d2) <- pairs ds, d1 == d2]
+  where pairs :: [a] -> [(a,a)]
+        pairs xs = zip xs (drop offset (xs ++ xs))
+
+checksumHalfWay :: [Int] -> Int
+checksumHalfWay ds = checksum (length ds `div` 2) ds
+
+runInput :: ([Int] -> Int) -> IO ()
+runInput f =  do
+  result <- parseFromFile (many1 numericDigit) "input/1.txt"
+  either (error . show) (print . f) result
+  where numericDigit :: Parser Int
+        numericDigit = digit >>= \d -> return (read [d])
 
 day01 :: IO ()
 day01 = do
-  result <- parseFromFile (many1 numericDigit) "input/1.txt"
-  either (error . show) (print . checksum) result
-  where numericDigit :: Parser Int
-        numericDigit = digit >>= \d -> return (read [d])
+  putStrLn "Part 1"
+  runInput (checksum 1)
+  putStrLn "Part 2"
+  runInput checksumHalfWay
