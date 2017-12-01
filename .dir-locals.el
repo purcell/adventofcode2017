2e8b31cf8012c0759e1e22a75c1290db9f6b28bd
Steve Purcell <steve@sanityinc.com>
2017-12-01 20:19:57 +1300

    Reformat with hindent, use Data.Char.digitToInt

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (hindent-reformat-buffer-on-save . t)
+  (haskell-stylish-on-save)))
+
+
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,27 +1,25 @@
 module Day01
-    ( day01
-    ) where
+  ( day01
+  ) where
 
+import Data.Char
 import Text.Parsec
 import Text.Parsec.String
-
-
 
 checksum :: (Eq a, Num a) => Int -> [a] -> a
 checksum _ [] = 0
 checksum offset ds = sum [d1 | (d1, d2) <- pairs ds, d1 == d2]
-  where pairs :: [a] -> [(a,a)]
-        pairs xs = zip xs (drop offset (xs ++ xs))
+  where
+    pairs :: [a] -> [(a, a)]
+    pairs xs = zip xs (drop offset (xs ++ xs))
 
 checksumHalfWay :: [Int] -> Int
 checksumHalfWay ds = checksum (length ds `div` 2) ds
 
 runInput :: ([Int] -> Int) -> IO ()
-runInput f =  do
-  result <- parseFromFile (many1 numericDigit) "input/1.txt"
+runInput f = do
+  result <- parseFromFile (many1 (digitToInt <$> digit)) "input/1.txt"
   either (error . show) (print . f) result
-  where numericDigit :: Parser Int
-        numericDigit = digit >>= \d -> return (read [d])
 
 day01 :: IO ()
 day01 = do
