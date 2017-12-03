ac2d325e303b70c689bdc2818f891f0f17aa5e6d
Steve Purcell <steve@sanityinc.com>
2017-12-03 21:57:01 +1300

    Day 03b

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day03
   ( day03
   ) where
 
+import qualified Data.Map as M
+import Data.Maybe (catMaybes)
 import qualified Data.Set as S
 
 data Direction
@@ -10,30 +14,55 @@ data Direction
   | S
   | W
 
-positionOf :: Int -> (Int, Int)
+type Pos = (Int, Int)
+
+turnLeft :: Direction -> Direction
+turnLeft N = W
+turnLeft W = S
+turnLeft S = E
+turnLeft E = N
+
+move :: Direction -> Pos -> Pos
+move N (x, y) = (x, y + 1)
+move S (x, y) = (x, y - 1)
+move E (x, y) = (x + 1, y)
+move W (x, y) = (x - 1, y)
+
+positionOf :: Int -> Pos
 positionOf n = positions !! (n - 1)
 
-positions :: [(Int, Int)]
+positions :: [Pos]
 positions = (\(p, _, _) -> p) <$> iterate next ((0, 0), S, S.singleton (0, 0))
   where
-    next ((x, y), dir, seen) =
+    next (pos, dir, seen) =
       if leftPos `S.notMember` seen
         then (leftPos, left, S.insert leftPos seen)
-        else (move dir, dir, S.insert (move dir) seen)
+        else (move dir pos, dir, S.insert (move dir pos) seen)
       where
-        leftPos = move left
+        leftPos = move left pos
         left = turnLeft dir
-        turnLeft N = W
-        turnLeft W = S
-        turnLeft S = E
-        turnLeft E = N
-        move N = (x, y + 1)
-        move S = (x, y - 1)
-        move E = (x + 1, y)
-        move W = (x - 1, y)
+
+sumValues :: [Int]
+sumValues = fst <$> scanl totalAt (1, M.singleton startPos 1) rest
+  where
+    (startPos:rest) = positions
+    totalAt (_, sums) pos = (thisVal, M.insert pos thisVal sums)
+      where
+        thisVal =
+          sum . catMaybes . fmap (`M.lookup` sums) . neighbourPositions $ pos
+
+neighbourPositions :: Pos -> [Pos]
+neighbourPositions (x, y) =
+  [ (x', y')
+  | x' <- [x - 1, x, x + 1]
+  , y' <- [y - 1, y, y + 1]
+  , (x, y) /= (x', y')
+  ]
 
 day03 :: IO ()
 day03 = do
   putStrLn "Part 1"
   let (x, y) = positionOf 347991
   print $ abs x + abs y
+  putStrLn "Part 2"
+  print $ head $ dropWhile (<= 347991) sumValues
