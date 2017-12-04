59b88f24c93126fb9a3a65739fc4289d9d4bfe95
Steve Purcell <steve@sanityinc.com>
2017-12-04 18:42:24 +1300

    Day 04b

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,7 +2,9 @@ module Day04
   ( day04
   ) where
 
-import Data.List (nub)
+import Data.List (nubBy)
+import Data.Map (Map)
+import qualified Data.Map as M
 import Prelude hiding (Word, words)
 import Text.Parsec
 import Text.Parsec.String (Parser, parseFromFile)
@@ -11,8 +13,14 @@ type Phrase = [Word]
 
 type Word = String
 
-valid :: Phrase -> Bool
-valid words = length words == length (nub words)
+validBy :: (Word -> Word -> Bool) -> Phrase -> Bool
+validBy comparator words = length words == length (nubBy comparator words)
+
+isAnagramOf :: Word -> Word -> Bool
+isAnagramOf a b = charHistogram a == charHistogram b
+
+charHistogram :: Word -> Map Char Int
+charHistogram = foldr (M.unionWith (+)) M.empty . fmap (`M.singleton` 1)
 
 withInput :: FilePath -> Parser a -> IO a
 withInput path p = do
@@ -26,4 +34,6 @@ day04 :: IO ()
 day04 =
   withInput "input/4.txt" parser >>= \parsed -> do
     putStrLn "Part 1"
-    print $ length $ filter valid parsed
+    print $ length $ filter (validBy (==)) parsed
+    putStrLn "Part 2"
+    print $ length $ filter (validBy isAnagramOf) parsed
