fd78425d84ec31b7227b4f20d82ad95684c73cdd
Steve Purcell <steve@sanityinc.com>
2017-12-06 19:51:07 +1300

    Part 06b

--- src/Day06.hs
+++ src/Day06.hs
@@ -17,23 +17,36 @@ redistribute banks = V.accum (+) banks' additions
     maxVal = banks ! maxPos
     banks' = banks // [(maxPos, 0)]
 
-part1 :: [Int] -> Int
-part1 blockCounts = go S.empty (iterate redistribute (V.fromList blockCounts))
+part1 :: Vector Int -> Int
+part1 = fst . countUntilRepeated . redistributions
+
+part2 :: Vector Int -> Int
+part2 blockCounts = fst . countUntilRepeated $ fromFirstRepeat
   where
-    go seen (x:xs)
-      | x `S.notMember` seen = 1 + go (S.insert x seen) xs
-    go _ _ = 0
+    fromFirstRepeat = snd . countUntilRepeated . redistributions $ blockCounts
+
+redistributions :: Vector Int -> [Vector Int]
+redistributions = iterate redistribute
+
+countUntilRepeated :: [Vector Int] -> (Int, [Vector Int])
+countUntilRepeated = go S.empty 0
+  where
+    go seen n (x:xs)
+      | x `S.notMember` seen = go (S.insert x seen) (n + 1) xs
+    go _ n xs = (n, xs)
 
 withInput :: FilePath -> Parser a -> IO a
 withInput path p = do
   result <- parseFromFile (p <* eof) path
   either (error . show) return result
 
-parser :: Parser [Int]
-parser = sepBy1 (read <$> many1 digit) (char '\t') <* newline
+parser :: Parser (Vector Int)
+parser = V.fromList <$> sepBy1 (read <$> many1 digit) (char '\t') <* newline
 
 day06 :: IO ()
 day06 =
   withInput "input/6.txt" parser >>= \parsed -> do
     putStrLn "Part 1"
     print $ part1 parsed
+    putStrLn "Part 2"
+    print $ part2 parsed
