a197a9f2c06156482d68736ae605c70920843d1d
Steve Purcell <steve@sanityinc.com>
2017-12-12 21:43:37 +1300

    Day 12b

--- src/Day12.hs
+++ src/Day12.hs
@@ -7,8 +7,10 @@ module Day12
 import Control.Arrow ((&&&))
 import Data.Graph.Inductive.Basic (undir)
 import Data.Graph.Inductive.Graph (LNode, mkGraph)
+import qualified Data.Graph.Inductive.Graph as G
 import Data.Graph.Inductive.PatriciaTree
 import Data.Graph.Inductive.Query.BFS (bfs)
+import qualified Data.Set as S
 import Text.Parsec
 import Text.Parsec.String (Parser, parseFromFile)
 
@@ -22,6 +24,16 @@ makeGraph pipes = undir $ mkGraph nodes edges
 
 toNode :: Int -> LNode Int
 toNode = (id &&& id)
+
+subgraphCount :: Network -> Int
+subgraphCount g = go 0 S.empty (G.nodes g)
+  where
+    go n _ [] = n
+    go n seen (x:xs)
+      | x `S.member` seen = go n seen xs
+    go n seen (x:xs) = go (n + 1) seen' xs
+      where
+        seen' = seen `S.union` S.fromList (bfs x g)
 
 withInput :: FilePath -> Parser a -> IO a
 withInput path p = do
@@ -44,3 +56,5 @@ day12 =
   withInput "input/12.txt" parser >>= \parsed -> do
     putStrLn "Part 1"
     print $ length $ bfs 0 $ makeGraph parsed
+    putStrLn "Part 2"
+    print $ subgraphCount $ makeGraph parsed
